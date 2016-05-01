6ed15c3cc1b3dd6ca8c606c37e9dd96c2b88df73
carles <carlescs@gmail.com>
2016-05-01 22:09:07 +0200

    * poincer.cs: * packages.config: * MainPage.xaml: * poincer.iOS.csproj: * MainPage.xaml.cs: * poincer.projitems: * poincer.Droid.csproj: * CalculatorPage.xaml: * Resource.designer.cs: * CalculatorPage.xaml.cs: * CalculatorViewModel.cs:
    * packages.config: Initial version of calculator.

---
+++ poincer/Pages/CalculatorPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace poincer
+{
+	public partial class CalculatorPage : ContentPage
+	{
+		public CalculatorPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
---
+++ poincer/ViewModels/CalculatorViewModel.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace poincer.ViewModels
+{
+	public class CalculatorViewModel:INotifyPropertyChanged
+	{
+		public CalculatorViewModel ()
+		{
+			
+		}
+
+		decimal fat;
+		public decimal Fat {
+			get {
+				return fat;
+			}
+			set {
+				if (fat != value) {
+					fat = value;
+					OnPropertyChanged (nameof (Fat));
+					OnPropertyChanged (nameof (Points));
+				}
+			}
+		}
+
+		decimal carbohydrates;
+		public decimal Carbohydrates {
+			get {
+				return carbohydrates;
+			}
+			set {
+				if (carbohydrates != value) {
+					carbohydrates = value;
+					OnPropertyChanged (nameof (Carbohydrates));
+					OnPropertyChanged (nameof (Points));
+				}
+			}
+		}
+
+		decimal fibre;
+		public decimal Fibre {
+			get {
+				return fibre;
+			}
+			set {
+				if (fibre != value) {
+					fibre = value;
+					OnPropertyChanged (nameof (Fibre));
+					OnPropertyChanged (nameof (Points));
+				}
+			}
+		}
+
+		decimal protein;
+		public decimal Protein {
+			get {
+				return protein;
+			}
+			set {
+				if (protein != value) {
+					protein = value;
+					OnPropertyChanged (nameof (Protein));
+					OnPropertyChanged (nameof (Points));
+				}
+			}
+		}
+
+		public decimal Points {
+			get {
+				return Math.Max((16m * Protein + 19m * Carbohydrates + 45m * Fat + 5m * Fibre) / 175m, 0);
+			}
+		}
+
+
+		#region INotifyPropertyChanged implementation
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+
+		protected virtual void OnPropertyChanged (string propertyName)
+		{
+			var handler = PropertyChanged;
+			if (handler != null)
+				handler (this, new PropertyChangedEventArgs(propertyName));
+		}
+		#endregion
+	}
+}
+
