90941c37bedd80edc98f09d54fde90217530db46
Carles Company Soler <carles.cs@outlook.com>
2016-05-01 19:54:23 +0200

    Add UWP project.

---
+++ poincer.UWP/Localize.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(poincer.UWP.Localize))]
+namespace poincer.UWP
+{
+    public class Localize : Localization.Util.ILocalize
+    {
+        public CultureInfo GetCurrentCultureInfo()
+        {
+            return CultureInfo.DefaultThreadCurrentCulture;
+        }
+    }
+}
