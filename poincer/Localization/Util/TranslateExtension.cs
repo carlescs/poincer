ad242cbe9cbf9eb7384c4b5fd8920ebc411d4a50
Carles Company Soler <ccompany@bbsoftbalear.com>
2016-05-03 13:59:38 +0200

    Fix file layout.

--- poincer/Localization/Util/TranslateExtension.cs
+++ poincer/Localization/Util/TranslateExtension.cs
@@ -1,46 +1,44 @@
 ﻿using System;
+using System.Globalization;
+using System.Reflection;
+using System.Resources;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using System.Resources;
-using System.Reflection;
-using System.Globalization;
-using poincer.Localization.Util;
 
 namespace poincer.Localization.Util
 {
-	[ContentProperty ("Text")]
-	public class TranslateExtension : IMarkupExtension
-	{
-	    private readonly CultureInfo _ci;
-		const string ResourceId = "poincer.Localization.AppResources";
+    [ContentProperty("Text")]
+    public class TranslateExtension : IMarkupExtension
+    {
+        private const string ResourceId = "poincer.Localization.AppResources";
+        private readonly CultureInfo _ci;
 
-		public TranslateExtension() 
-		{
-			_ci = DependencyService.Get<ILocalize>().GetCurrentCultureInfo ();
-		}
+        public TranslateExtension()
+        {
+            _ci = DependencyService.Get<ILocalize>().GetCurrentCultureInfo();
+        }
 
-		public string Text { get; set; }
+        public string Text { get; set; }
 
-		public object ProvideValue (IServiceProvider serviceProvider)
-		{
-			if (Text == null)
-				return "";
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Text == null)
+                return "";
 
-			ResourceManager resmgr = new ResourceManager(ResourceId
-				, typeof(TranslateExtension).GetTypeInfo().Assembly);
+            var resmgr = new ResourceManager(ResourceId
+                , typeof(TranslateExtension).GetTypeInfo().Assembly);
 
-			var translation = resmgr.GetString (Text, _ci);
+            var translation = resmgr.GetString(Text, _ci);
 
-			if (translation == null) 
-			{
-				#if DEBUG
-			    throw new ArgumentException($"Key '{Text}' was not found in resources '{ResourceId}' for culture '{_ci.Name}'.", nameof(Text));
+            if (translation == null)
+            {
+#if DEBUG
+                throw new ArgumentException($"Key '{Text}' was not found in resources '{ResourceId}' for culture '{_ci.Name}'.", nameof(Text));
 #else
 				translation = Text; // HACK: returns the key, which GETS DISPLAYED TO THE USER
-				#endif
-			}
-			return translation;
-		}
-	}
+#endif
+            }
+            return translation;
+        }
+    }
 }
-
